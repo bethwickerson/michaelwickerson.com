53a1edce9dfb221e5540d2b4737621624c0bc808
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-01-28 10:01:41 -0600

    node 14.15

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/bethwickerson/Desktop/michaelwickerson.github.io/react-portfolio-template/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/bethwickerson/Desktop/michaelwickerson.github.io/react-portfolio-template/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/bethwickerson/Desktop/michaelwickerson.github.io/react-portfolio-template/src/pages/index.js")))
 }

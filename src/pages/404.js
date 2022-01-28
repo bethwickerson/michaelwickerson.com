ed9293d81ac818f109ebd31dcff38588217a7b19
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-01-28 11:52:45 -0600

    revise 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,15 @@ import Seo from "../components/Seo"
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="section" id="home">
+        <div className="container" style={{
+          padding: "10rem 0",
+        }}>
+          <h1>404: Not Found</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <a href="/">Go back home</a>
+        </div>
+    </div>
   </Layout>
 )
 

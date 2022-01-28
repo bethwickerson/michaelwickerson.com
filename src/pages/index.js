ee4d0bc945e877dbfd4eae0bc6c01474640caf9d
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-01-28 14:04:52 -0600

    metadata fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Portfolio Template" />
+    <SEO title="Michael Wickerson - Life's Work" />
     <Header></Header>
     <Work></Work>
     <About></About>

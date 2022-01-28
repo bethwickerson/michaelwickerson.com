bad8284df0d0f23e8c4a295d3737fb5839a92eed
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-01-28 14:12:31 -0600

    metadata duplicate title fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Michael Wickerson - Life's Work`,
+    title: `Wickerson Studios`,
     description: `CEO and Co-founder at Wickerson Studios and Former Chair of Sculpture at Kansas City Art Institute`,
     author: `@bethwickerson`,
   },
